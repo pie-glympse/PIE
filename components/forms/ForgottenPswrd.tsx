609925228691cecee96940eedf099e64c5f7e2f5
By-Kvn <kevin.labatte0@gmail.com>
2025-06-20 22:30:02 +0200

    feat(frgtenpswrd comp): add arrow back

--- components/forms/ForgottenPswrd.tsx
+++ components/forms/ForgottenPswrd.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent } from 'react';
 import MainButton from '../ui/MainButton';
+import Image from 'next/image';
 
 interface ForgottenPswrdProps {
     title: string;
@@ -25,6 +26,20 @@ const ForgottenPswrd: React.FC<ForgottenPswrdProps> = ({
 
     return (
         <form onSubmit={handleSubmit} className="w-full mx-auto">
+            <button
+                type="button"
+                onClick={() => window.location.href = '/'}
+                className="bg-[var(--color-text)] rounded-md w-10 h-10 flex justify-center items-center mb-8 cursor-pointer"
+                aria-label="Back"
+            >
+                <Image
+                    src="/icons/BackArrow.svg"
+                    alt="Back"
+                    width={18}
+                    height={17}
+                    className="mx-auto"
+                />
+            </button>
             <h1 className="text-h1 mb-8 text-left w-2/3 font-urbanist">{title}</h1>
             <div className="mb-4">
                 <label htmlFor="email" className="block mb-1 text-body-large font-poppins text-[var(--color-grey-three)]">Adresse e-mail</label>
