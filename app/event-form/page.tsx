f759396f5e47e0a8c2e5162004c915e1abac25f6
By-Kvn <kevin.labatte0@gmail.com>
2025-06-25 22:57:12 +0200

    feat(event form): add it

---
+++ app/event-form/page.tsx
@@ -0,0 +1,33 @@
+"use client" // src/app/register/page.tsx
+import EventForm from '@/components/forms/EventForm';
+import BackArrow from '@/components/ui/BackArrow';
+
+export default function EventFormPage() {
+
+    return (
+        <section className="flex flex-row h-screen items-center gap-10 p-10">
+            <div className="h-full w-full flex flex-col justify-between items-start gap-6 p-10">
+                <div className="w-full flex flex-col justify-center">
+                    <div>
+                        <BackArrow onClick={() => window.history.back()} className="" />
+                    </div>
+                    <EventForm
+                        title={
+                            <>
+                                Créez vos évènements personnalisés !<br />
+                            </>
+                        }
+                        subtitle='Entrez les informations générales de l’événement'
+                        buttonText="Créer l'événement"
+                        placeholderTitle="Titre de l'événement"
+                        placeholderDescription="Description de l'événement"
+                        placeholderDateRange="Ex: 01/01/2024 - 07/01/2024"
+                        placeholderMaxBudget="Ex: 50€"
+                        placeholderCity="Ville"
+                        placeholderMaxDistance="Ex: 50km"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
