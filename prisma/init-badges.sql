b3efb1280a68271876d6000052253e55fae19a8c
Marvin <marvin.clerc@edu.devinci.fr>
2026-05-21 23:40:48 +0200

    feat: add badge

--- prisma/init-badges.sql
+++ prisma/init-badges.sql
@@ -4,10 +4,10 @@
 -- Insérer les 4 badges de base
 INSERT INTO "Badge" (id, name, description, icon, "pointsRequired", "order", "createdAt")
 VALUES 
-  (1, 'Robuste Diplomate', 'Complétez votre première action et rejoignez l''aventure', '/images/illustration/joy.png', 0, 1, NOW()),
-  (2, 'Explorateur étoilé', 'Gagnez 100 points en participant activement à des événements', '/images/illustration/magic.png', 100, 2, NOW()),
-  (3, 'Harmoniste enjoleur', 'Atteignez 500 points et repandez l''amour autour de vous', '/images/illustration/sad.png'  , 500, 3, NOW()),
-  (4, 'Chuchoteur de Paix', 'Obtenez 1000 points et rejoignez la cour des legendes', '/images/illustration/base.png', 1000, 4, NOW())
+  (1, 'Robuste Diplomate', 'Complétez votre première action et rejoignez l''aventure', '/images/badges/badge_star.png', 0, 1, NOW()),
+  (2, 'Explorateur étoilé', 'Gagnez 100 points en participant activement à des événements', '/images/badges/badge_flash.png', 100, 2, NOW()),
+  (3, 'Harmoniste enjoleur', 'Atteignez 500 points et repandez l''amour autour de vous', '/images/badges/badge_hypnoz.png', 500, 3, NOW()),
+  (4, 'Chuchoteur de Paix', 'Obtenez 1000 points et rejoignez la cour des legendes', '/images/badges/badge_peacemaker.png', 1000, 4, NOW())
 ON CONFLICT (name) DO NOTHING;
 
 -- Exemple: Ajouter des points à un utilisateur et créer l'historique
