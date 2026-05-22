9ae8517136322a7cf95fb707e046c6a1a59a3929
ambre.kouitini <ambrektn@outlook.fr>
2026-05-22 14:33:11 +0200

    feat: add settings page

--- components/Header.tsx
+++ components/Header.tsx
@@ -211,7 +211,7 @@ export default function Header() {
                 <span className="font-medium">Entreprise</span>
               </Link>
 
-              {/* <Link
+              <Link
                 href="/settings"
                 className="flex items-center px-4 py-3 text-gray-700 hover:bg-gray-50 hover:text-secondary transition-colors duration-200 group"
               >
@@ -225,11 +225,17 @@ export default function Header() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8c-.519-.598-1.489-1-2.599-1m0 0C8.014 7 6 8.343 6 10c0 .656.164 1.28.451 1.818M10.5 21h3m-3 0a4.5 4.5 0 01-4.5-4.5v-3m7.5 7.5a4.5 4.5 0 004.5-4.5v-3m-7.5 7.5h3"
+                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                 </svg>
                 <span className="font-medium">Paramètres</span>
-              </Link> */}
+              </Link>
 
               <Link
                 href="/profile"
